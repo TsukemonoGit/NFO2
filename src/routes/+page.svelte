1ed27c974d36cbf9124358449cfec55b282a34f8
TsukemonoGit <mine43try@outlook.com>
2026-06-01 17:40:59 +0900

    so-to?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,17 @@
   import { hexRegex } from "$lib/utils/regex";
   import { sortFollowList } from "$lib/utils/sortFollowers";
   import { Select } from "bits-ui";
-  import { Check, Triangle } from "@lucide/svelte";
-  import { untrack } from "svelte";
+  import {
+    Check,
+    Triangle,
+    Users,
+    type LucideProps,
+    FilePenLine,
+    List,
+    Tags,
+    Tag,
+  } from "@lucide/svelte";
+  import { untrack, type Component } from "svelte";
   import { SortOrder } from "$lib/types";
   import { Toggle } from "bits-ui";
 
@@ -19,9 +28,24 @@
     [SortOrder.myPetname]: "MyPetname",
     [SortOrder.theirPetname]: "TheirPetname",
   };
-
-  const themes = Object.values(SortOrder).map((value) => {
-    return { value: value as string, label: labels[value], disabled: false };
+  interface OrderData {
+    value: string;
+    label: string;
+    icon: Component<LucideProps, {}, "">;
+  }
+  const orderData: OrderData[] = [
+    {
+      value: "follow",
+      label: "Follow",
+      icon: List,
+    },
+    { value: "latestPost", label: "Latest Post", icon: FilePenLine },
+    { value: "mutual", label: "Mutual", icon: Users },
+    { value: "myPetname", label: "myPetname", icon: Tags },
+    { value: "theirPetname", label: "theirPetname", icon: Tag },
+  ];
+  const themes = orderData.map((data) => {
+    return { value: data.value, label: data.label };
   });
 
   $effect(() => {
@@ -40,7 +64,7 @@
 
 <h1 class="text-lg">Nostr Follow Organizer 2</h1>
 {#if latestKind3.value}
-  <div class="flex items-center">
+  <div class="flex items-center gap-2 mb-2">
     <Select.Root
       type="single"
       items={themes}
@@ -48,15 +72,14 @@
       bind:value={sortOrder as never}
     >
       <Select.Trigger
-        class="h-input rounded-md border-border-input bg-background data-placeholder:text-foreground-alt/50 inline-flex w-[296px] touch-none select-none items-center border p-1 my-2 text-sm transition-colors"
+        class="inline-flex h-10 w-74 items-center rounded-md border border-outline bg-background p-1 text-sm transition-colors "
         aria-label="Select a theme"
       >
         <Select.Value placeholder="Select a sort order" />
       </Select.Trigger>
       <Select.Portal>
         <Select.Content
-          class="focus-override border-muted bg-background shadow-popover data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 outline-hidden z-50 h-96 max-h-[var(--bits-select-content-available-height)] w-[var(--bits-select-anchor-width)] min-w-[var(--bits-select-anchor-width)] select-none rounded-xl border px-1 py-3 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1"
-          sideOffset={10}
+          class="z-50  w-74 border border-outline bg-background shadow-lg rounded-xl select-none px-1 py-3 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
         >
           <Select.ScrollUpButton
             class="flex w-full items-center justify-center"
@@ -64,14 +87,14 @@
             <!-- <CaretDoubleUp class="size-3" /> -->
           </Select.ScrollUpButton>
           <Select.Viewport class="p-1">
-            {#each themes as theme, i (i + theme.value)}
+            {#each orderData as theme, i (theme.value)}
               <Select.Item
-                class="rounded-button data-highlighted:bg-muted outline-hidden data-disabled:opacity-50 flex h-10 w-full select-none items-center py-3 pl-5 pr-1.5 text-sm capitalize"
+                class="flex h-10 w-full select-none items-center rounded-md px-5 py-3 text-sm capitalize data-highlighted:bg-surface-container data-disabled:opacity-50 gap-2"
                 value={theme.value}
                 label={theme.label}
-                disabled={theme.disabled}
               >
                 {#snippet children({ selected })}
+                  <theme.icon />
                   {theme.label}
                   {#if selected}
                     <div class="ml-auto">
@@ -92,7 +115,7 @@
     </Select.Root>
     <Toggle.Root
       aria-label="toggle code visibility"
-      class="bg-background-alt hover:bg-muted active:bg-dark-10 data-[state=on]:bg-muted data-[state=off]:text-foreground-alt data-[state=on]:text-foreground active:data-[state=on]:bg-dark-10 inline-flex size-10 items-center justify-center rounded-[9px] transition-all active:scale-[0.98]"
+      class="inline-flex h-10 w-10 items-center justify-center rounded-full border border-outline bg-background transition-colors hover:bg-surface-container data-[state=on]:bg-surface-container  active:scale-[0.98]"
       bind:pressed={reverse}
     >
       <Triangle rotate={180} class={[reverse ? "rotate-180" : ""]} />
